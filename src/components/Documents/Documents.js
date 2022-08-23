5b055afca0bc0c1fabeff1c78f4a1fbbf08b7a31
Koomin <bartoszkominko@gmail.com>
2022-08-23 23:58:59 +0200

    Move DocumentsTable to DocumentsTable.js

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -1,12 +1,10 @@
-import MaterialTable from "material-table";
 import React, { useEffect, useState } from "react";
-import { getDocumentItems, getDocuments } from "../../services/documentsService";
-import { DocumentDialog } from "./DocumentDialog";
+import { getDocuments } from "../../services/documentsService";
 
-function Documents({handleChangePage,handleChangeRowsPerPage}) {
+import { DocumentsTable } from "./DocumentsTable";
+
+function Documents() {
     const [documents, setDocuments] = useState([]);
-    const [documentItems, setDocumentItems] = useState([]);
-    const [documentsData, setDocumentsData] = useState([]);
     const [isOpenDocumentDialog, setOpenDocumentDialog] = useState(false);
 
     const fetchDocuments = async () => {
@@ -15,62 +13,17 @@ function Documents({handleChangePage,handleChangeRowsPerPage}) {
         setDocuments(documents);
     };
 
-    const fetchDocumentItems = async (id) => {
-        const response = await getDocumentItems(id);
-        const documentItems = response.data;
-        setDocumentItems(documentItems);
-    };
-
-    const handleClickOpen = async (event, data) => {
-        if( data)
-        await setDocumentsData(data);
-        await fetchDocumentItems(data.id);
-        setOpenDocumentDialog(true);
-    };
-
-    const handleClose = (shouldFetch = false) => {
-        setOpenDocumentDialog(false);
-        setDocumentsData([]);
-        setDocumentItems([]);
-        if (shouldFetch) {
-          fetchDocuments();
-        }
-      };
-
     useEffect(() => {
         fetchDocuments();
     }, []);
 
     return (
-        <div>
-            <MaterialTable 
-            columns = {[
-                {title: "Numer Optima", field: "optima_full_number"},
-                {title: "Wartość Netto", field: "value_net"},
-                {title: "Wartość Brutto", field: "value_gross"},
-                {title: "Magazyn źródłowy", field: "source_store"},
-                {title: "Magazyn docelowy", field: "destination_store"},
-                {title: "Grupa dokumentu", field: "document_group"},
-                {title: "Typ dokumentu", field: "document_type"},
-                {title: "Wyeksportowane", field: "exported"},
-            ]}
-            options={{
-                pageSize: 10,
-                pageSizeOptions: [ 10 ]
-            }}
-            onRowClick={handleClickOpen}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-            data={documents}
-            title="Dokumenty"
-            />
-            <DocumentDialog
-                handleClose={() => handleClose(false)}
-                documentsData={documentsData}
-                documentItems={documentItems}
-                open={isOpenDocumentDialog}
-            />
-        </div>  
+        <DocumentsTable 
+            documents={documents}
+            isOpenDocumentDialog={isOpenDocumentDialog}
+            fetchDocuments={fetchDocuments}
+            setOpenDocumentDialog={setOpenDocumentDialog}
+        />
     );
 }
 
---
+++ src/components/Documents/DocumentsTable.js
@@ -0,0 +1,68 @@
+import React, { useState }from "react";
+import MaterialTable from "material-table";
+import { DocumentDialog } from "./DocumentDialog";
+import { getDocumentItems } from "../../services/documentsService"
+
+export function DocumentsTable({
+    handleChangePage,
+    handleChangeRowsPerPage,
+    documents,
+    fetchDocuments,
+    isOpenDocumentDialog,
+    setOpenDocumentDialog}) {
+
+        const [documentsData, setDocumentsData] = useState([]);
+        const [documentItems, setDocumentItems] = useState([]);
+
+        const fetchDocumentItems = async (id) => {
+            const response = await getDocumentItems(id);
+            const documentItems = response.data;
+            setDocumentItems(documentItems);
+        };
+
+        const handleClickOpen = async (event, data) => {
+            await setDocumentsData(data);
+            await fetchDocumentItems(data.id);
+            setOpenDocumentDialog(true);
+        };
+    
+        const handleClose = (shouldFetch = false) => {
+            setOpenDocumentDialog(false);
+            setDocumentsData([]);
+            setDocumentItems([]);
+            if (shouldFetch) {
+              fetchDocuments();
+            }
+          };
+        return (
+            <div>
+                <MaterialTable 
+                columns = {[
+                    {title: "Numer Optima", field: "optima_full_number"},
+                    {title: "Wartość Netto", field: "value_net"},
+                    {title: "Wartość Brutto", field: "value_gross"},
+                    {title: "Magazyn źródłowy", field: "source_store"},
+                    {title: "Magazyn docelowy", field: "destination_store"},
+                    {title: "Grupa dokumentu", field: "document_group"},
+                    {title: "Typ dokumentu", field: "document_type"},
+                    {title: "Wyeksportowane", field: "exported"},
+                ]}
+                options={{
+                    pageSize: 10,
+                    pageSizeOptions: [ 10 ]
+                }}
+                onRowClick={handleClickOpen}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                data={documents}
+                title="Dokumenty"
+                />
+                <DocumentDialog
+                    handleClose={() => handleClose(false)}
+                    documentsData={documentsData}
+                    documentItems={documentItems}
+                    open={isOpenDocumentDialog}
+                />
+            </div>  
+        );
+};
